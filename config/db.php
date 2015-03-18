9dcfb5559ca22f66ae185fb33933bcd9a6914b66
Sergey Francev <sergey.francev@eurostudio.net>
2015-03-18 12:52:36 +0600

    Init project

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=dota2club',
+    'username' => 'root',
+    'password' => 'repol',
+    'charset' => 'utf8',
+];
